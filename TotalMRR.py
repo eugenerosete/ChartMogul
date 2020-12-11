a852ab27ca0309335645fabb2fbb9044157ed2cc
Eugene Rosete <eugenerosete@gmail.com>
2020-12-11 21:26:48 +0800

    Added code to print summary report using the Metrics object values.

--- TotalMRR.py
+++ TotalMRR.py
@@ -1,4 +1,5 @@
 import chartmogul
+import calendar
 
 config = chartmogul.Config('0bf0c27f6492ce2ce8d88c5ca9ea6bba', '7fa2c0e0515d1b916997a42db98fd675')
 
@@ -8,5 +9,6 @@ data = chartmogul.Metrics.mrr(
     end_date="2019-04-30",
     interval="month")
 
-jsonMsg = data.value
-print(jsonMsg, end="\n\n")
+print("Total MRR for Q1 2019", end="\n\n")
+for entry in data.value.entries:
+    print(calendar.month_name[entry.date.month] + ": " + str(entry.mrr))
